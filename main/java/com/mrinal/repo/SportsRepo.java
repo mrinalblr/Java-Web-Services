1a866d5cf501675d5277bae8912add67e624ce2b
Mrinal Deo (COMMUNICATIONS) <ME765564@wipro.com>
2017-05-04 18:18:13 +0530

    Changes

---
+++ main/java/com/mrinal/repo/SportsRepo.java
@@ -0,0 +1,14 @@
+package com.mrinal.repo;
+
+import java.util.ArrayList;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mrinal.model.Sports;
+
+public interface SportsRepo extends CrudRepository<Sports, Integer> {
+	
+	public Sports findBySportsName(String sportsName);
+	public ArrayList<Sports> findAllBySportsName(String sportsName);
+}
